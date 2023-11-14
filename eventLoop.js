e53994e713a3a60e710b290fc17a87b1053a876b
Shreyas <sonureigns007@gmail.com>
2023-11-14 21:59:10 +0530

    micro task example process.nextTick()

--- eventLoop.js
+++ eventLoop.js
@@ -7,10 +7,12 @@ fs.readFile("test-file.txt", () => {
   }, 3000); //phase 1
   setImmediate(() => {
     console.log("immediate2 finished");
-  }); //phase 2 // nut inside this call back this should be executed first because after entering into the i/o poll phase (reading file here) it will waits in the same face until the next timeout expiry to start the cycle agin but here because the immediate is called after i/o (reading file) the "SETIMMEDIATE" takes the priority ove set timeout because it ii the next phase after I/O
+  }); //phase 2
+    // but inside this call back this is executed first even before setTimeout 0because after entering into the i/o poll phase (reading file here) it will waits in the same face until the next timeout expiry to start the cycle agin but here because the immediate is called after i/o (reading file) the "SETIMMEDIATE" takes the priority ove set timeout because it ii the next phase after I/O
     setTimeout(() => {
     console.log("timer2 finished");
-  }, 0); //phase 1
+    }, 0); //phase 1
+    process.nextTick(() => console.log('im am the mvp here')) // this is the micro task which will executed immediately after every phase hence this will take more priority than setImmediate
 }); //phase 3
 setImmediate(() => {
   console.log("immediate1 finished");
